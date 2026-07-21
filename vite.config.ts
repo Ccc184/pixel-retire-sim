f7c39165e18ad0fd42bf0eb9d1efb0ac15d2822a
Ccc184 <Ccc184@users.noreply.github.com>
2026-07-21 21:50:44 +0800

    清理冗余文件、添加游戏设计文档

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: './',
-  plugins: [
-    vue(),
-    tailwindcss(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      renderModernChunks: false,
-    }),
-  ],
+  plugins: [vue(), tailwindcss()],
 })
